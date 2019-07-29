c5cf437f1e7dad61ac0d3e423378c1478007379d
Armine <dan.armine@gmail.com>
2019-07-29 10:35:55 -0700

    add_sub_categories

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './styles/App.css';
 import Categories from './Components/Categories';
+import PrimarySearchAppBar from './Components/header/header';
 import Login from './Components/header/login';
 
 
@@ -8,8 +9,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-
-          <Login/>
+        <PrimarySearchAppBar/>
+        {/*  <Login/>*/}
           <Categories/>
       </header>
     </div>
--- src/firebase/fireManager.js
+++ src/firebase/fireManager.js
@@ -4,9 +4,29 @@ export function getActiveCategories() {
     return new Promise((resolve, reject)=>{
         db.collection('Categories').where("status", "==", true).get().then((snapshot)=>{
             const data = [];
+            const id=[];
+
+            snapshot.docs.forEach(doc=>{
+                const obj={...doc.data(),id:doc.id}
+                data.push(obj);
+              // id.push(doc.id);
+               // console.log(doc.data())
+            });
+           resolve(data)
+
+        }).catch(e=> reject(e));
+    })
+}
+
+export function getsubCategories(id) {
+    return new Promise((resolve, reject)=>{
+
+        db.collection('Categories').doc(id).collection('sub').get().then((snapshot)=>{
+            const data = [];
+            //debugger;
             snapshot.docs.forEach(doc=>{
                 data.push(doc.data());
-            })
+            });
             resolve(data)
         }).catch(e=> reject(e));
     })

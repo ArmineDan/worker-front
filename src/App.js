115ff963d3adb60ff21b18bdf8c26dc5b5045424
Shushan Hovhannisayn <shushan_hov@yahoo.com>
2019-08-03 12:32:17 +0400

    finishWorkers

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './styles/App.css';
-import Categories from './Components/Categories';
-import PrimarySearchAppBar from './Components/header/header';
-import Login from './Components/header/login';
-import Register from'./Components/header/register'
+// import Categories from './Components/Categories';
+// import PrimarySearchAppBar from './Components/header/header';
+// import Login from './Components/header/login';
+// import Register from'./Components/header/register'
+import MediaCard from './Components/workers'
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <PrimarySearchAppBar/>
-          <Categories/>
+        <MediaCard/>
+          {/* <PrimarySearchAppBar/>
+          <Categories/> */}
           {/*<Register/>*/}
           {/*<Login/>*/}
       </header>
